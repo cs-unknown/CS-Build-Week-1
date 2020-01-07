b6876994cab8947fd90f7277d9af86c6aca73055
Dan <danbergelt@protonmail.com>
2020-01-06 20:50:05 -0500

    switching to psql

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -93,16 +93,8 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if(config('DEBUG', cast=bool) == True):
-  DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-else:
-  import dj_database_url
-  DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
+import dj_database_url
+DATABASES = {'default': dj_database_url.config(default=config('DATABASE_URL'))}
 
 
 
